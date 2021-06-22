ccb7d85ebc58a95e3ee3787d6ba5e5e3521b0f7c
Sara <ss.toughoj@gmail.com>
2021-06-22 19:43:17 +0300

    EPISODE 3: Testing Request Validation

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Project;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -25,5 +26,25 @@ class ProjectsTest extends TestCase
 
         $this->get('/projects')
             ->assertSee($attributes['title']);
+
+    }
+
+    /** @test */
+    public function a_project_requires_a_title()
+    {
+        $attributes = Project::factory()->raw(['title' => null]);
+
+        $this->post('/projects', $attributes)
+            ->assertSessionHasErrors('title');
+    }
+
+    /** @test */
+    public function a_project_requires_a_description()
+    {
+
+        $attributes = Project::factory()->raw(['description' => null]);
+
+        $this->post('/projects', $attributes)
+            ->assertSessionHasErrors('description');
     }
 }
