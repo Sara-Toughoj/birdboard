8fc4125c7f30c970d7d1bdae4ce40704ef860782
Sara <ss.toughoj@gmail.com>
2021-06-26 21:46:51 +0300

    EPISODE 8: Prepping the Frontend

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,15 +1,33 @@
-<h1>
-    Create a Project
-</h1>
-<div>
-    <form action="/projects" method="post">
-        @csrf
-        <div>
-            <input name="title"/>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Birdboard
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <h1>
+                        Create a Project
+                    </h1>
+                    <div>
+                        <form action="/projects" method="post">
+                            @csrf
+                            <div>
+                                <input name="title"/>
+                            </div>
+                            <div>
+                                <textarea name="description"></textarea>
+                            </div>
+                            <button type="submit"> Submit</button>
+                            <a href="/projects"> cancel</a>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div>
-            <textarea name="description"></textarea>
-        </div>
-        <button type="submit"> Submit</button>
-    </form>
-</div>
+    </div>
+
+</x-app-layout>
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,12 +1,32 @@
-<h1> Birdboard</h1>
-<ul>
-    @forelse($projects as $project)
-        <li>
-            <a href="{{$project->path()}}">
-                {{$project->title}}
+<x-app-layout>
+    <x-slot name="header">
+        <div class="flex items-center">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight mr-auto mb-4">
+                Birdboard
+            </h2>
+            <a href="/projects/create">
+                Create New Projects
             </a>
-        </li>
-    @empty
-        <li> No Projects yet</li>
-    @endforelse
-</ul>
+        </div>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <ul class="list-disc text-blue-700">
+                        @forelse($projects as $project)
+                            <li>
+                                <a href="{{$project->path()}}" class="underline ">
+                                    {{$project->title}}
+                                </a>
+                            </li>
+                        @empty
+                            <li> No Projects yet</li>
+                        @endforelse
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
