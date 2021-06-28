3c12c2b8ead2f2c5d9667e7f3b69bd967d52b3ec
Sara <ss.toughoj@gmail.com>
2021-06-28 20:17:02 +0300

    EPISODE 9: Go Go Gadget Tailwind

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,31 +1,24 @@
 <x-app-layout>
-    <x-slot name="header">
-        <div class="flex items-center">
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight mr-auto mb-4">
-                Birdboard
-            </h2>
-            <a href="/projects/create">
-                Create New Projects
-            </a>
-        </div>
-    </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <ul class="list-disc text-blue-700">
-                        @forelse($projects as $project)
-                            <li>
-                                <a href="{{$project->path()}}" class="underline ">
-                                    {{$project->title}}
-                                </a>
-                            </li>
-                        @empty
-                            <li> No Projects yet</li>
-                        @endforelse
-                    </ul>
-                </div>
+            <a href="/projects/create">
+                New Project
+            </a>
+            <div class="flex">
+                @forelse($projects as $project)
+                    <div class="bg-white mr-4 rounded-lg shadow-lg w-1/3 p-4 h-50">
+                        <div class="text-xl mb-4 py-4">
+                            {{Str::limit($project->title ,50)}}
+                        </div>
+                        <div class="mb-3 text-gray-400">
+                            {{Str::limit($project->description , 100)}}
+                        </div>
+                    </div>
+                @empty
+                    <div>
+                        No Projects Yet.
+                    </div>
+                @endforelse
             </div>
         </div>
     </div>
